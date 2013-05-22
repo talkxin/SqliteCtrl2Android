da87845a21faa9de92c189216a6cb0d293e3509f
talkxin <talkxin@gmail.com>
2013-05-22 08:45:34 +0800

    初始化项目

---
+++ src/com/sqliteCtrl/Annotation/TableProperty.java
@@ -0,0 +1,17 @@
+package com.sqliteCtrl.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TableProperty {
+	/**
+	 * 是否会被映射入结果集
+	 * 
+	 * @return
+	 */
+	boolean toObject() default true;
+}
